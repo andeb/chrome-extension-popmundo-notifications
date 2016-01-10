210e123a276dff7a89d34d046760daba1dcf68cd
Anderson de Borba <ande.brb@gmail.com>
2016-01-10 20:20:27 -0200

    Removing old files.

--- amazing.js
+++
@@ -1,8 +0,0 @@
-  		function n() {
-  			var notification = new Notification('Notification title', {
-      			icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
-      			body: "Hey there! You've been notified!",
-			});
-
-  			chrome.notifications.create(1, notification);
-  		}
